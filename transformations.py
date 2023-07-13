ec763e0a5ed624aa9a912bfbe679d0bd16be49f2
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-07-13 10:32:20 +0900

    transformations script

--- transformations.py
+++ transformations.py
@@ -1,6 +1,21 @@
+# Import library
 import albumentations as A
 
 def get_transformations(size):
+
+    """
+    
+    This function gets input image size and returns train and test transformations to be applied.
+
+    Parameter:
+
+        size      - input image size, int.
+
+    Output:
+
+        out       - train and test transformations, list.
+    
+    """
 
     return [A.Compose([A.Resize(size, size),
                       A.HorizontalFlip(0.5),
@@ -8,5 +23,4 @@ def get_transformations(size):
                       A.GaussNoise(0.2),
                       ]),
            
-            A.Compose([A.Resize(size, size)])
-           ]
+            A.Compose([A.Resize(size, size)])]
