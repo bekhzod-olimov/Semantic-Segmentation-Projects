6b73bbf4f893ebfa8f11fae1c95326119857173c
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-08-02 01:26:42 +0900

    Update main.py

--- sam/main.py
+++ sam/main.py
@@ -1,7 +1,6 @@
 # Import libraries
 import torch, wandb, argparse, yaml, os, pickle, pytorch_lightning as pl
-from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import WandbLogger; from pytorch_lightning.callbacks.early_stopping import EarlyStopping
 from pytorch_lightning.callbacks import ModelCheckpoint, Callback
 from dataset import get_dls; from transformers import SamProcessor
 from time import time; from model import LitModel, ImagePredictionLogger
