2533d3425c7698ce85bb3b8b284a7834d49b5fb7
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2023-09-14 11:25:46 +0900

    Update segformer.py

--- models/segformer.py
+++ models/segformer.py
@@ -169,18 +169,27 @@ class SegFormerEncoderBlock(nn.Sequential):
 
 
 class SegFormerEncoderStage(nn.Sequential):
-    def __init__(
-        self,
-        in_channels: int,
-        out_channels: int,
-        patch_size: int,
-        overlap_size: int,
-        drop_probs: List[int],
-        depth: int = 2,
-        reduction_ratio: int = 1,
-        num_heads: int = 8,
-        mlp_expansion: int = 4,
-    ):
+
+    """
+
+    This class gets several parameters and conducts encoder stage.
+
+    Parameters:
+
+        in_channels           - number of input channels in the input volume, int;
+        out_channels          - number of output channels in the convolution layer, int;
+        patch_size            - patch size, int;
+        overlap_size          - overlap size, int;
+        drop_probs            - dropout layer probabilities, list -> int;
+        depth                 - depth of the block, int;
+        reduction_ratio       - reduction ratio value, int;
+        num_heads             - number of heads in the encoder, int;
+        mlp_expansion         - mlp expansion ratio, int.
+        
+    """
+    
+    def __init__(self, in_channels: int, out_channels: int, patch_size: int, overlap_size: int, drop_probs: List[int], 
+                 depth: int = 2, reduction_ratio: int = 1, num_heads: int = 8, mlp_expansion: int = 4):
         super().__init__()
         self.overlap_patch_merge = OverlapPatchMerging(
             in_channels, out_channels, patch_size, overlap_size,
