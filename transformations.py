6959f3ec4e8d1e5c049880f4174848c82a167f93
Bekhzod Olimov <50166164+bekhzod-olimov@users.noreply.github.com>
2024-09-26 15:43:53 +0900

    transformations script

--- transformations.py
+++ transformations.py
@@ -17,10 +17,6 @@ def get_transformations(size):
     
     """
 
-    return [A.Compose([A.Resize(size, size),
-                      A.HorizontalFlip(0.5),
-                      A.VerticalFlip(0.5),
-                      A.GaussNoise(0.2),
-                      ]),
-           
+    return [A.Compose([A.Resize(size, size), A.HorizontalFlip(0.5),
+                      A.VerticalFlip(0.5),   A.GaussNoise(0.2)]),
             A.Compose([A.Resize(size, size)])]
